ba25f74bd6b6c9d134f1fea27d3e7517e9005f42
Francis Miguel Castillo Cruz <franciscastillocruz@gmail.com>
2025-02-01 15:18:04 -0400

    Agregar validación de esquemas para registro y inicio de sesión en las rutas de autenticación

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,19 +1,39 @@
 import { Hono } from "hono";
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator'
+
+const registerSchema = z.object({
+    email: z.string().trim().toLowerCase().email({
+        message: "Invalid email address"
+    }),
+    password: z.string().min(8, { message: "Invalid password"}),
+    username: z.string().min(3, { message: "Invalid username"}),
+});
+
+const loginSchema = z.object({
+    email: z.string().trim().toLowerCase().email({
+        message: "Invalid email address"
+    }),
+    password: z.string().trim().toLowerCase().min(8, { message: "Invalid password"}),
+});
 
 export const authRouter = new Hono();
 
-// /api/v1/auth/login
-authRouter.post("/login", (c) => {
+// /api/v1/auth/register
+authRouter.post("/register", zValidator("json", registerSchema), async (c) => {
+
+    const { email, password, username } = await c.req.json();
+
     return c.json({
-        message: "Login"
+        email, password, username
     });
 })
 
-// /api/v1/auth/register
-authRouter.post("/register", (c) => {
-    return c.json({
-        message: "Register"
-    });
+// /api/v1/auth/login
+authRouter.post("/login", zValidator("json", loginSchema), async (c) => {
+    const { email, password } = await c.req.json();
+
+    return c.json({ email, password });
 })
 
 export default authRouter;
