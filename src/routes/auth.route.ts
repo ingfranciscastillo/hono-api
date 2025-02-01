c307c292b0e91a482f909d8101cc2a706c5c5d74
Francis Miguel Castillo Cruz <franciscastillocruz@gmail.com>
2025-02-01 17:21:45 -0400

    Agregar verificación de existencia de usuario y registro en la ruta de autenticación

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,6 +2,10 @@ import { Hono } from "hono";
 import { z } from 'zod';
 import { zValidator } from '@hono/zod-validator'
 
+import { db } from "../db";
+import { usersTable } from "../db/schema";
+import { eq } from "drizzle-orm";
+
 const registerSchema = z.object({
     email: z.string().trim().toLowerCase().email({
         message: "Invalid email address"
@@ -23,6 +27,25 @@ export const authRouter = new Hono();
 authRouter.post("/register", zValidator("json", registerSchema), async (c) => {
 
     const { email, password, username } = await c.req.json();
+
+    const user = await db.select().from(usersTable).where(eq(usersTable.email, email));
+
+    if (user.length > 0) {
+        return c.json({
+            error: "User already exists"
+        }, 400);
+    }
+
+    const bscryptHash = await Bun.password.hash(password, {
+        algorithm: "bcrypt",
+        cost: 4,
+    })
+
+    await db.insert(usersTable).values({
+        email,
+        password: bscryptHash,
+        username,
+    });
 
     return c.json({
         email, password, username
